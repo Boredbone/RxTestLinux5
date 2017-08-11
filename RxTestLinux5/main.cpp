090e9e89273e276777635d993547bbaa8826a8dc
boredbone <Boredbone@users.noreply.github.com>
2017-08-11 19:23:18 +0900

    RxCpp

--- RxTestLinux5/main.cpp
+++ RxTestLinux5/main.cpp
@@ -1,7 +1,37 @@
 #include <cstdio>
 
+#include <iostream>
+#include <string>
+
+#include "rxcpp/rx.hpp"
+
+
+namespace rx
+{
+	using namespace rxcpp;
+	using namespace rxcpp::sources;
+	using namespace rxcpp::operators;
+	using namespace rxcpp::util;
+}
+using namespace rx;
+
 int main()
 {
-    printf("hello from RxTestLinux5!\n");
-    return 0;
+	std::cout << "start" << std::endl;
+
+	rx::sources::range(1, 6)
+		.take(3)
+		.subscribe(
+			[](int i) { std::cout << i << std::endl; },
+			[]() { std::cout << "completed" << std::endl; });
+
+	std::cout << "end" << std::endl;
+
+	{
+		int a;
+		std::cin >> a;
+	}
+
+
+	return 0;
 }
